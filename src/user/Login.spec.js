0c431a256db05f12d8c8dc6d6a417fc18ba71a74
Xavi <javiergimenezsanchez@gmail.com>
2020-01-31 20:15:26 +0100

    removed unnused lines

--- src/user/Login.spec.js
+++ src/user/Login.spec.js
@@ -1,4 +1,4 @@
-import React, { useReducer }  from "react";
+import React  from "react";
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
 import { idText, isExportDeclaration } from "typescript";
