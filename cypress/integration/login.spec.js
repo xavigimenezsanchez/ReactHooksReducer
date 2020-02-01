2ced15b940556f3e5df8cf7045dfb8c9946acbdc
Xavi <javiergimenezsanchez@gmail.com>
2020-02-01 10:12:57 +0100

    add more expects

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,9 +1,22 @@
+import { isExportDeclaration } from "typescript";
+
 describe('My First Test', function() {
     it('Loggin works', function() {
+      const user = "User Test"
       cy.visit('http://localhost:3000')
   
-      cy.get('#login-username').type('hola');
+      cy.get('#login-username').type(user);
       cy.get('#login-password').type('patata');
       cy.get(':nth-child(1) > form > .btn').click()
+
+      cy.get('.jumbotron > .btn').should(($p) => {
+        expect($p).to.contain('Logout')
+      });
+
+      cy.get('p > b').should((x) =>{
+        expect(x).to.contain(user)
+      })
+
+      cy.get('.badge').should(x=>expect(x).to.contain(user))
     })
   })
