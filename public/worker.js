e80fb0b5aeca40eb436dcb764f6beadebcc300f1
Xavi <javiergimenezsanchez@gmail.com>
2020-03-03 20:35:49 +0100

    start with pwa

---
+++ public/worker.js
@@ -0,0 +1,13 @@
+
+self.addEventListener('install', function(e) {
+    console.log('Install......')
+   });
+   
+   self.addEventListener('fetch', function(e) {
+     console.log(e.request.url);
+     e.respondWith(
+       caches.match(e.request).then(function(response) {
+         return response || fetch(e.request);
+       })
+     );
+   });
